0a4e1c24526f5a97e5095d18f14a37689894bff5
Dittrich Ákos <dittrich.kos@gmail.com>
2024-05-08 23:33:19 +0200

    add missing image dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,6 +99,9 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
+    //Image
+    implementation("io.coil-kt:coil-compose:2.0.0-rc01")
+
 }
 
 kapt {
